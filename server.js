dba5569f0e2f410b848e22b7902b6547f1007afe
hallspaugh <hunterallspaugh@Hunters-MacBook-Pro.local>
2021-06-09 20:56:46 -0500

    Fixed duplicate result sets with sfdx-node/parallel

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const port = 3000;
 const orgs = require('./server/routes/orgs');
 const login = require('./server/routes/login');
 
-const Connection = require('@salesforce/core');
-console.log(Connection);
-
 app.use(express.json());
 app.use(express.static('public')); 
 app.use('/slds', express.static(__dirname + '/node_modules/@salesforce-ux/design-system/assets'));
--- server/routes/orgs.js
+++ server/routes/orgs.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const sfdx = require('sfdx-node');
+const sfdx = require('sfdx-node/parallel');
 const router = express.Router();
 
 router.get('/', (request, response, next) => {
   sfdx.force.org.list()
     .then(orgs => {
       response.send(orgs);
-      console.log(orgs);
+      console.log(orgs.nonScratchOrgs.length);
     })
     .catch(error => response.error(error));
 });
