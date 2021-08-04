9ae327893e4c5d4fc918ab5881508c3a23f7f81c
Hunter Allspaugh <hunter.allspaugh@sproutsocial.com>
2021-08-04 14:34:33 -0500

    Added temporary variable and value for default dev hub to allow scratch org creation

--- server/routes/orgs.js
+++ server/routes/orgs.js
@@ -4,6 +4,7 @@ const sfdx = require('sfdx-node/parallel');
 const router = express.Router();
 const path = require('path');
 const scratchOrgConfig = path.resolve("config/project-scratch-def.json");
+const targetDevHubName = 'production';
 
 router.get('/', (request, response, next) => {
   sfdx.force.org.list()
@@ -18,7 +19,8 @@ router.post('/', (request, response, next) => {
   console.log(scratchOrgConfig);
   sfdx.force.org.create({
     setalias: request.body.alias,
-    definitionfile: scratchOrgConfig
+    definitionfile: scratchOrgConfig,
+    targetdevhubusername: targetDevHubName
   })
     .then(status => {
       response.send(status);
